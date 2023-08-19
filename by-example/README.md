42e7d66e523902952dfc839364f93b176852ff1d
Eduardo Pereira Garcia <edupgarcia.ti@gmail.com>
2023-08-19 19:42:02 -0300

    Projects imported

---
+++ by-example/README.md
@@ -0,0 +1,2 @@
+# Rust by example
+In this project I use the basics of Rust language which I learned from Rust by Example from [Rust](rust-lang.org) website.
---
+++ by-example/src/main.rs
@@ -0,0 +1,9 @@
+// mod ferris;
+// mod formatted_print;
+mod debug;
+
+fn main() {
+    // println!("Hello, world!"); ferris::main();
+    // formatted_print::main();
+    debug::first(); debug::second();
+}
